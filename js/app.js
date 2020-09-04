79e28bae75cc668f794819e32b639a2eddbf6550
Rockchalk8985 <steven.foster10@gmail.com>
2020-09-04 00:14:49 -0500

    done with assignment

---
+++ js/app.js
@@ -0,0 +1,28 @@
+import {render} from "./personalProfile.js"
+import {Person} from "./personalProfile.js" 
+
+let people = [
+    new Person("Bob", "bob-ross.jpg"),
+    new Person("Frida", "frida-kahlo.jpg"),
+    new Person("Dali", "dali.jpg")
+];
+
+window.onload = () => {
+    renderList(people);
+};
+
+function renderList(people) {
+    let html = "";
+    html += `<ul>`;
+    for (let person of people) {
+        html += "<li>";
+        html += render(person);
+        html += "</li>";
+    }
+    html += "</ul>";
+
+    document.getElementById("list").innerHTML = html;
+
+}
+render(person);
+
--- js/personalProfile.js
+++ js/personalProfile.js
@@ -1,15 +1,15 @@
-
-class Person {
+export class Person {
     constructor(name, imageUrl) {
         this.name = name;
         this.imageUrl = imageUrl;
     }
 }
 
-function render(person) {
+
+export function render(person) {
     return `
         <div class="profile shadow">
-            <img src="${person.imageUrl}"/>
+            <img src="./Images/${person.imageUrl}"/>
             <h2>${person.name}</h2>
         </div>
     `
